4d5a20074d179ef1c2c3f5b57bf11a6b4b9c1f45
Ismail <salmi.ismail8u@gmail.com>
2019-12-15 21:42:53 +0100

    refactor

--- socialape-functions/.history/functions/util/handlers/users_20191215212336.js
+++
@@ -1,3 +0,0 @@
-const { db } = require('../admin');
-const firebase = require('firebase');
-firebase.initializeApp(config)
--- socialape-functions/.history/functions/util/validators_20191215213322.js
+++
@@ -1,8 +0,0 @@
-const isEmpty = (string)=>{
-    return (string.trim() === '');
-  }
-  
-  const isEmail = (email) => {
-    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return email.match(regEx);
-  }
